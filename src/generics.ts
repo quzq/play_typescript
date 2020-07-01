57a439d95a6b4866b60f5b4bd5ee3fddb869d555
quzq <quzq@outlook.jp>
2020-07-01 16:05:08 +0900

    generics sample

--- src/generics.ts
+++ src/generics.ts
@@ -1,4 +1,12 @@
-export default ():void=>{
+export default () => {
+  console.log('--- generics.ts -----------------------------')
+  console.log(`* `)
+  function test<T>(arg: T): T {
+    return arg;
+  }
 
+  console.log(test<number>(1)); //=> 1
+  console.log(test<string>("string")); //=> 文字列
+  
 
 }
