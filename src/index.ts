7e9835456ce9d8d5bb2a75eb89c319e0da836224
quzq <quzq@outlook.jp>
2020-07-01 13:47:15 +0900

    add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import classes from './class'
 import Class2 from './class2'
-
-classes()
-
-const foo = new Class2(1,1)
-foo.log()
+import generics from "./generics"
+//classes()
+//const foo = new Class2(1,1)
+//foo.log()
+generics()
