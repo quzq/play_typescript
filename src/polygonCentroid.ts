576f43f3089999ecfd8bb32d832a517d443e1475
quzq <quzyam@gmail.com>
2021-01-23 10:47:40 +0900

    refactor

--- src/polygonCentroid.ts
+++ src/polygonCentroid.ts
@@ -19,11 +19,9 @@ export default () => {
       p2,
       f;
     for (let i = 0, j = nPts - 1; i < nPts; j = i++) {
-      console.log(i, j)
       p1 = pts[i];
       p2 = pts[j];
-      f =
-        (p1.y - first.y) * (p2.x - first.x) - (p2.y - first.y) * (p1.x - first.x);
+      f = (p1.y - first.y) * (p2.x - first.x) - (p2.y - first.y) * (p1.x - first.x);
       twicearea += f;
       x += (p1.x + p2.x - 2 * first.x) * f;
       y += (p1.y + p2.y - 2 * first.y) * f;
@@ -39,12 +37,7 @@ export default () => {
     const pts = (first.x != last.x || first.y != last.y) ? [..._pts, first] : _pts
 
     const nPts = pts.length;
-    let twicearea = 0,
-      x = 0,
-      y = 0,
-      p1,
-      p2,
-      _f;
+    let twicearea = 0, x = 0, y = 0
     for (let i = 0, j = nPts - 1; i < nPts; j = i++) {
       const p1 = pts[i];
       const p2 = pts[j];
